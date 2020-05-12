Fixed numerical error in perfect matching function

python_code/Cell_Diagram_Drawer.py
@@ -62,12 +62,19 @@ def show_image(window, image):
     cv.moveWindow(window, W, 200)
     cv.waitKey(0)
     cv.destroyAllWindows()
-  
+
+def boolean_subset_unranker(j, N):   
+    answer = ''    
+    for i in range(1, N+1):
+        if j %2 == 1:
+            answer += str(i)
+        j = j //2    
+    return answer
 
 #for mode, all seperate means that the program will display each cell one at a time
 #combined means that the program will display all cells at once
 #showImages tells whether or not the images should be displayed on the screen
-def print_cell_diagram(mode, showImages):
+def print_cell_diagram(mode, showImages, useColorInImages, intersectCellsWithSupMu):
     
     if mode == "all seperate" and not os.path.exists('Cell_Diagrams'):
         os.makedirs('Cell_Diagrams')
@@ -77,7 +84,11 @@ def print_cell_diagram(mode, showImages):
     cellWindow = "Cell Diagram"
     cellImage = np.full(size, 255, dtype=np.uint8)
     
-    cellDataFile = open(cellDataFileLocation, 'r')
+    if intersectCellsWithSupMu:   
+        cellDataFile = open(cellDataWithMuFileLocation, 'r') 
+    else:
+        cellDataFile = open(cellDataFileLocation, 'r')
+    
     N = int(read_line(cellDataFile))
     
     '''
@@ -90,10 +101,17 @@ def print_cell_diagram(mode, showImages):
     '''
     
     for j in range(1, 2**N):
+        
+        if intersectCellsWithSupMu:   
+            read_line(cellDataFile) 
+            
         count = int(read_line(cellDataFile))
         
-        color = (rand.randint(0, 255), rand.randint(0, 255), rand.randint(0, 255))
-        
+        if useColorInImages:
+            color = (rand.randint(0, 255), rand.randint(0, 255), rand.randint(0, 255))
+        else:
+            color = (100,100,100)
+            
         for i in range(count):
             x0, x1, y0, y1 = read_rectangle(cellDataFile)
             cv.rectangle(cellImage, (x0,y0), (x1,y1), color, -1, 8)
@@ -101,7 +119,7 @@ def print_cell_diagram(mode, showImages):
         if(mode == "all seperate"):
             if(showImages):
                 show_image(cellWindow, cellImage)
-            cv.imwrite("Cell_Diagrams/" + "Cell_Diagram"+ str(j) +".png", cellImage)
+            cv.imwrite("Cell_Diagrams/" + "Cell_Diagram"+ boolean_subset_unranker(j,N) +".png", cellImage)
             cellImage = np.full(size, 255, dtype=np.uint8)
     
     if(mode == "combined"):
@@ -111,7 +129,7 @@ def print_cell_diagram(mode, showImages):
     
     cellDataFile.close()
 
-def print_transport_diagram():
+def print_transport_diagram(showImages):
     
     if not os.path.exists('Warehouse_Diagrams'):
         os.makedirs('Warehouse_Diagrams')
@@ -146,7 +164,8 @@ def print_transport_diagram():
                 cv.rectangle(warehouseImages[k], (cell_rectangles[i][0],cell_rectangles[i][2]), (cell_rectangles[i][1],cell_rectangles[i][3]), (shade, shade, shade), -1, 8)
     
     for k in range(N):
-        show_image(transportWindow, warehouseImages[k])     
+        if(showImages):
+            show_image(transportWindow, warehouseImages[k])     
         cv.imwrite("Warehouse_Diagrams/" + "Warehouse_Diagram" + str(k + 1) + ".png", warehouseImages[k])
     
     cellDataWithMuFile.close()
@@ -155,8 +174,8 @@ def print_transport_diagram():
     
 
 #start main program
-print_transport_diagram()
-print_cell_diagram("all seperate", False)
+print_transport_diagram(False)
+print_cell_diagram("all seperate", False, False, True)
 
 
 